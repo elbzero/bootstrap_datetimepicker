b6edd396b8ce2b2d7353d5e8f77dc2f27d2b5d7b
Gary Foster <garywaynefoster@gmail.com>
2014-09-02 07:42:23 -0400

    Fix exception when nil

--- app/inputs/datetime_local_input.rb
+++ app/inputs/datetime_local_input.rb
@@ -2,7 +2,7 @@ class DatetimeLocalInput < SimpleForm::Inputs::StringInput
   def input(wrapper_options = nil)
     input_html_options[:type] = 'datetime-local'
     input_html_options[:html5] = true
-    input_html_options[:value] ||= value(object).strftime('%FT%R')
+    input_html_options[:value] = value(object) ? value(object).strftime('%FT%R') : nil
 
     merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
 
