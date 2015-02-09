bf9dc67767dcd59106f2d415dbfcc37bcf5f83e8
Eric Bundy <elbzero@gmail.com>
2015-02-09 09:10:03 -0500

    field testing

--- app/inputs/datetime_local_input.rb
+++ app/inputs/datetime_local_input.rb
@@ -1,14 +1,16 @@
 class DatetimeLocalInput < SimpleForm::Inputs::StringInput
   def input(wrapper_options = nil)
-    @builder.text_field(attribute_name, input_html_options)
+    input_html_options[:type] = 'datetime-local'
+    input_html_options[:html5] = true
+    input_html_options[:title] = "Test9"
+    input_html_options[:value] = value(object) ? value(object).strftime('%FT%R') : nil
+
+    merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
+
+    @builder.text_field(attribute_name, merged_input_options)
   end
 
   def value(object)
     object.send @attribute_name if object
   end
-
-  def input_html_options
-    {class: 'form-control', readonly: true, type: "date", title: "test7"}
-  end
-
 end
